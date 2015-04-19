ef8003c3652ab51d225c73de851a58bdf81891b7
akpandeya <akpandeya1@gmail.com>
2015-04-19 10:44:36 +0530

    Update procar.py

--- procar.py
+++ procar.py
@@ -1,12 +1,12 @@
 import sys
 if ((len(sys.argv) != 2)):
-	print "Usage: python procar.py ion_number \n Example: python lmdos.py 3 \nIt will print the lm decomposed DOS for ion number 3 in POSCAR. \nRequires PROCAR file to be present"
+	print "Usage: python parse_procar.py ion_number \n Example: python parse_procar.py 3 \nIt will print the lm decomposed DOS for ion number 3 in POSCAR. \nRequires PROCAR file to be present"
 	exit(1)
 try:
 	int(sys.argv[1])
 except:
 	print "The second argument should be integer. If you are writing element name, it won't work. Check POSCAR to find put the ion number."
-	print "Usage: python procar.py ion_number \n Example: python lmdos.py 3 \nIt will print the lm decomposed DOS for ion number 3 in POSCAR. \nRequires PROCAR file to be present"
+	print "Usage: python lmdos.py ion_number \n Example: python lmdos.py 3 \nIt will print the lm decomposed DOS for ion number 3 in POSCAR. \nRequires PROCAR file to be present"
 	exit(2)
 readion = int(sys.argv[1])
 try:
@@ -15,15 +15,15 @@ except IOError:
 	print "The file PROCAR could not be read. There could be many reasons for this. \n1)You did not run vasp with correct LORBIT option. \n2)Reading or writing files is not permmitted. You can try sudo in this case."
 	print "Exiting program"
 	exit(3)
-f.readline()
-s = f.readline()
-number_list = s.split()
+line1 = f.readline()
+line2 = f.readline()
+number_list = line2.split()
 nkpoint = int(number_list[3])
 nbands = int(number_list[7])
 nions = int(number_list[11])
-s = f.readline()
-s = f.readline().split()
-kpoint = [float(s[3]), float(s[4]), float(s[5])]
+blankline = f.readline()
+line4 = f.readline().split()
+kpoint = [float(line4[3]), float(line4[4]), float(line4[5])]
 try:
 	f1 = open(str(readion)+"lm.dat", "w")
 except IOError:
@@ -31,11 +31,13 @@ except IOError:
 	print "Exiting program"
 	exit(4)
 for i in range(0, nbands):
-	s = f.readline()
-	s = f.readline().split()
-	energy = float(s[4])
+	blankline = f.readline()
+	energy_line = f.readline().split()
+	energy = energy_line[4]
 	f.readline()
 	if (i == 0):
+		f1.write("Energy")
+		f1.write("             ")
 		head = f.readline().split()
 		for item in head[1:]:
 			f1.write(item)
@@ -43,6 +45,8 @@ for i in range(0, nbands):
 		f1.write('\n')
 	else:
 		f.readline()
+	f1.write(energy)
+	f1.write("       ")
 	for j in range(0, nions+1):
 		if(j == readion-1):
 			prodos = f.readline().split()
